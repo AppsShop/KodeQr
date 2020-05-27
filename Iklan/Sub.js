bbf4df0750c61157b0823557a2b6617e8b2b1774
Tikha <64693040+Sub-Olahoop@users.noreply.github.com>
2020-05-27 20:08:12 +0700

    Update Sub.js

--- Iklan/Sub.js
+++ Iklan/Sub.js
@@ -1,1 +1,3 @@
 <head><script data-ad-client="ca-pub-7201802427887817" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script></
+
+https://ad.atdmt.com/s/go;adv=11162240857546;ec=11162244167583;c.a={campaignid};s.a=google;p.a={campaignid};as.a={adgroupid};qpb=1;?bidkw={keyword:defaultkeyword}&dvc={device}&h={lpurl}
